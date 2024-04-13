d9182ce2d3de56b32a04f65f36a7549c8ddc6216
Léo Desktop <peyronnet.leo@gmail.com>
2024-04-13 12:24:20 +0200

    ajout enum Couleur

---
+++ src/Main.java
@@ -0,0 +1,6 @@
+import entities.Couleur;
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(Couleur.VERT.getCodeANSI()+"C'est tout bon chef !");
+    }
+}
---
+++ src/entities/Couleur.java
@@ -0,0 +1,34 @@
+package entities;
+
+public enum Couleur {
+    NOIR(1,"Noir","\u001B[30m"),
+    ROUGE(2,"Rouge","\u001B[31m"),
+    VERT(3,"Vert","\u001B[32m"),
+    JAUNE(4,"Jaune","\u001B[33m"),
+    BLEU(5,"Bleu","\u001B[34m"),
+    MAGENTA(6,"Magenta","\u001B[35m"),
+    CYAN(7,"Cyan","\u001B[36m"),
+    BLANC(8,"Blanc","\u001B[37m");
+
+    private int index;
+    private String nom;
+    private String codeANSI;
+
+    Couleur(int index, String nom, String codeANSI){
+        this.index = index;
+        this.nom = nom;
+        this.codeANSI = codeANSI;
+    }
+
+    public int getIndex() {
+        return index;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public String getCodeANSI() {
+        return codeANSI;
+    }
+}
