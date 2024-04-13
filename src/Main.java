2975455f37a93554d5fc132cc1fb891dd8279538
Léo Desktop <peyronnet.leo@gmail.com>
2024-04-13 17:53:05 +0200

    Main: structuration tests

--- src/Main.java
+++ src/Main.java
@@ -4,11 +4,14 @@ import entities.Pion;
 import entities.CombinaisonPions;
 public class Main {
     public static void main(String[] args) {
+        //Test Couleur
         System.out.println(Couleur.VERT.getCodeANSI()+"C'est tout bon chef !");
 
+        //Test Pion
         Pion monPion = new Pion(Couleur.BLEU);
         System.out.println(monPion);
 
+        //Test CombinaisonPions
         CombinaisonPions maCombinaison = new CombinaisonPions(6);
         maCombinaison.addPion(new Pion(Couleur.JAUNE));
         maCombinaison.addPion(new Pion(Couleur.MAGENTA));
