aba5298b3f3858130325fbf96c51e3424ca1a3b7
Léo Desktop <peyronnet.leo@gmail.com>
2024-04-14 13:31:49 +0200

    Pion: getCouleur() & equals()

--- src/entities/Pion.java
+++ src/entities/Pion.java
@@ -7,8 +7,29 @@ public class Pion {
         this.couleurPion = couleurPion;
     }
 
+    public Couleur getCouleurPion() {
+        return couleurPion;
+    }
+
     @Override
     public String toString() {
         return couleurPion.getCodeANSI()+"O"+Couleur.BLANC.getCodeANSI();
     }
+
+    @Override
+    public boolean equals(Object obj) {
+        // Vérification de la référence de l'objet
+        if (this == obj) {
+            return true;
+        }
+
+        // Vérification si l'objet passé est null ou n'est pas du même type
+        if (obj == null || getClass() != obj.getClass()) {
+            return false;
+        }
+
+        // Comparaison
+        return getCouleurPion() == ((Pion) obj).getCouleurPion();
+    }
 }
+
