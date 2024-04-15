8c923db4762e4a69967abe824b3f73ab5f9acd5a
Léo Desktop <peyronnet.leo@gmail.com>
2024-04-15 11:58:56 +0200

    Couleur: suppr attribut nom ; methode getByIndex

--- src/entities/Couleur.java
+++ src/entities/Couleur.java
@@ -1,33 +1,31 @@
 package entities;
 
 public enum Couleur {
-    NOIR(1,"Noir","\u001B[30m"),
-    ROUGE(2,"Rouge","\u001B[31m"),
-    VERT(3,"Vert","\u001B[32m"),
-    JAUNE(4,"Jaune","\u001B[33m"),
-    BLEU(5,"Bleu","\u001B[34m"),
-    MAGENTA(6,"Magenta","\u001B[35m"),
-    CYAN(7,"Cyan","\u001B[36m"),
-    BLANC(8,"Blanc","\u001B[37m");
+    NOIR(1,"\u001B[30m"),
+    ROUGE(2,"\u001B[31m"),
+    VERT(3,"\u001B[32m"),
+    JAUNE(4,"\u001B[33m"),
+    BLEU(5,"\u001B[34m"),
+    MAGENTA(6,"\u001B[35m"),
+    CYAN(7,"\u001B[36m"),
+    BLANC(8,"\u001B[37m");
 
     private final int index;
-    private final String nom;
     private final String codeANSI;
 
-    Couleur(int index, String nom, String codeANSI){
+    Couleur(int index, String codeANSI){
         this.index = index;
-        this.nom = nom;
         this.codeANSI = codeANSI;
     }
 
-    public int getIndex() {
-        return index;
+    public Couleur getByIndex(int index) {
+        for (Couleur couleur : Couleur.values()) {
+            if (couleur.index == index) {
+                return couleur;
+            }
+        }
+        throw new IllegalArgumentException("Index invalide : " + index);
     }
-
-    public String getNom() {
-        return nom;
-    }
-
     public String getCodeANSI() {
         return codeANSI;
     }
