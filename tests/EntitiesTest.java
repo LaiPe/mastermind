9b83bbcab7c9e47d33f24cfaf285eea0e5897c90
Léo Desktop <peyronnet.leo@gmail.com>
2024-04-15 12:26:47 +0200

    Couleur: getCodeANSI => toString (tests)

--- tests/EntitiesTest.java
+++ tests/EntitiesTest.java
@@ -5,7 +5,7 @@ import entities.Result;
 public class EntitiesTest {
     public static void main(String[] args) {
         //Test Couleur
-        System.out.println(Couleur.VERT.getCodeANSI()+"C'est tout bon chef !");
+        System.out.println(Couleur.VERT+"C'est tout bon chef !");
 
         //Test Pion
         Pion monPion = new Pion(Couleur.BLEU);
