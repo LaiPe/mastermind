2cb0a86d74afde547374be862fa4ec2c24bd08bd
Léo Desktop <peyronnet.leo@gmail.com>
2024-04-27 15:19:34 +0200

    RulesList: création

---
+++ tests/RulesListTest.java
@@ -0,0 +1,8 @@
+import listes.RulesList;
+
+public class RulesListTest {
+    public static void main(String[] args) {
+        RulesList liste = new RulesList();
+        liste.getElement(0).setValue(10);
+    }
+}
--- tests/RulesTest.java
+++ tests/RulesTest.java
@@ -5,25 +5,6 @@ import java.util.List;
 
 public class RulesTest {
     public static void main(String[] args) {
-        /*List<Rule> maListedeRules  = new ArrayList<>();
-        maListedeRules.add(new AffichageTexte("O"));
-        maListedeRules.add(new NbEssais(10));
-
-        Rule maRule = maListedeRules.get(0);
-        Object maValeurDeRule = maRule.getValue();
-
-        if (maRule instanceof RuleInteger) {
-            int monEntier = (int) maValeurDeRule;
-            System.out.println(monEntier);
-
-        } else if (maRule instanceof RuleBoolean) {
-            boolean monBooleen = (boolean) maValeurDeRule;
-            System.out.println(monBooleen);
-
-        } else {
-            System.err.println("erreur");
-        }*/
-
         TailleMaxCombinaison t = new TailleMaxCombinaison();
         t.setValue(4);
         System.out.println(t.getValue());
