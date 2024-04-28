53d23bbe7a315579c5f94cb0e4b0a32d67775072
Léo Desktop <peyronnet.leo@gmail.com>
2024-04-28 21:16:26 +0200

    suppr. RulesList

--- tests/RulesListTest.java
+++
@@ -1,8 +0,0 @@
-import listes.RulesList;
-
-public class RulesListTest {
-    public static void main(String[] args) {
-        RulesList liste = new RulesList();
-        liste.getElement(0).setValue("10");
-    }
-}
