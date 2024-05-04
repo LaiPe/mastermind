c04f1268c13dbca66339716c9cd2d73407076521
Léo Desktop <peyronnet.leo@gmail.com>
2024-05-04 22:26:28 +0200

    Result: ajout index

--- src/entities/Result.java
+++ src/entities/Result.java
@@ -1,15 +1,21 @@
 package entities;
 
 public enum Result {
-    VALIDE(Couleur.VERT + "\u2713" + Couleur.RESET),
-    COULEUR(Couleur.MAGENTA + "\u2248" + Couleur.RESET),
-    INVALIDE(Couleur.ROUGE + "\u2717" + Couleur.RESET);
+    VALIDE(Couleur.VERT + "\u2713" + Couleur.RESET, 2),
+    COULEUR(Couleur.MAGENTA + "\u2248" + Couleur.RESET,1),
+    INVALIDE(Couleur.ROUGE + "\u2717" + Couleur.RESET,0);
 
     private final String string;
-    Result(String string){
+    private final int index;
+    Result(String string, int index){
         this.string = string;
+        this.index = index;
     }
 
+    public int getIndex() {
+        return index;
+    }
+    
     @Override
     public String toString() {
         return string;
